a40f65ec1a8bce847b1a4fb0d91bb4596c3e3bec
Ticki <Ticki@users.noreply.github.com>
2015-12-16 12:20:05 +0100

    Start new buffer data structure

---
+++ buffer.rs
@@ -0,0 +1,116 @@
+use std::mem::swap;
+
+/// The buffer data structure, that Sodium is using.
+///
+/// This structure consists of two "subbuffers", which are just vectors over lines (defined by
+/// Strings). The split is where the cursor currently is.
+///
+/// The second buffer is in reverse order to get the particular efficiency we want.
+pub struct SplitBuffer {
+    before: Vec<String>,
+    after: Vec<String>,
+}
+
+impl SplitBuffer {
+    /// Create a new empty buffer
+    pub fn new() -> Self {
+        SplitBuffer {
+            before: vec![String::new()],
+            after: Vec::new(),
+        }
+    }
+
+    pub fn get_line(&self) -> &String {
+        self.before.last().expect("Unexpected condition (the first part of the split buffer is empty)")
+    }
+
+    pub fn get_line_mut(&mut self) -> &mut String {
+        self.before.last_mut().expect("Unexpected condition (the first part of the split buffer is empty)")
+    }
+
+    pub fn up(&mut self) -> bool {
+        if self.before.len() == 1 {
+            false
+        } else {
+            self.after.push(self.before.pop());
+            true
+        }
+    }
+
+    pub fn down(&mut self) -> bool {
+        if self.after.len() == 1 {
+            false
+        } else {
+            self.before.push(self.after.pop());
+            true
+        }
+    }
+
+    pub fn line(&self) -> usize {
+        self.before.len()
+    }
+
+    pub fn get_nth_line(&self, n: usize) -> Option<&String> {
+        if n < self.before.len() {
+            Some(&self.before[n])
+        } else if n < self.before.len() + self.after.len() {
+            Some(&self.after[n - self.before.len()])
+        } else {
+            None
+        }
+    }
+
+    pub fn get_nth_line_mut(&mut self, n: usize) -> Option<&mut String> {
+        if n < self.before.len() {
+            Some(&mut self.before[n])
+        } else if n < self.before.len() + self.after.len() {
+            Some(&mut self.after[n - self.before.len()])
+        } else {
+            None
+        }
+    }
+
+    pub fn remove_line(&mut self, n: usize) {
+        if n < self.before.len() {
+            self.before.remove(n);
+            Some(&mut self.before[n])
+        } else if n < self.before.len() + self.after.len() {
+            self.after.remove(n - self.before.len());
+        }
+    }
+
+    pub fn from_lines(vec: Vec<String>) -> SplitBuffer {
+        SplitBuffer {
+            before: vec,
+            after: Vec::new(),
+        }
+    }
+
+    pub fn goto(&mut self, y: usize) {
+        if y < self.line() {
+            for _ in 1..self.line() - y {
+                if !self.up() {
+                    break;
+                }
+            }
+        } else if y > self.line() {
+            for _ in 1..y - self.line() {
+                if !self.down() {
+                    break;
+                }
+            }
+        }
+    }
+
+    pub fn pop_line(&mut self) -> String {
+        self.before.expect("Unexpected condition (Popped the last line)")
+    }
+
+    pub fn len(&self) -> usize {
+        self.before.len() + self.after.len()
+    }
+
+    pub fn iter(&self) -> ::std::iter::Once<Vec<char>> {
+        self.after.iter().chain(self.before.iter().reverse())
+    }
+}
